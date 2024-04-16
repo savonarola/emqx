c891f54dbfa5ee2eda8fb3314c41b5c9f2a455a6
Ilya Averyanov <av@rubybox.dev>
2024-04-16 14:09:01 +0300

    chore(git): do not override existing git hooks

--- scripts/git-hooks-init.sh
+++ scripts/git-hooks-init.sh
@@ -8,14 +8,14 @@ fi
 
 mkdir -p ".git/hooks"
 
-if [ ! -L '.git/hooks/pre-push' ]; then
+if [ ! -f '.git/hooks/pre-push' ]; then
     ln -sf '../../scripts/git-hook-pre-push.sh' '.git/hooks/pre-push'
 fi
 
-if [ ! -L '.git/hooks/pre-commit' ]; then
+if [ ! -f '.git/hooks/pre-commit' ]; then
     ln -sf '../../scripts/git-hook-pre-commit.sh' '.git/hooks/pre-commit'
 fi
 
-if [ ! -L '.git/hooks/post-commit' ]; then
+if [ ! -f '.git/hooks/post-commit' ]; then
     ln -sf '../../scripts/git-hook-post-commit.sh' '.git/hooks/post-commit'
 fi
