b4a89e4f3bdc0860bd39928408da14933f0e15ee
Ilia Averianov <av@rubybox.dev>
2025-10-31 17:03:00 +0200

    wip

---
+++ apps/emqx_extsub_redis/src/emqx_extsub_redis.erl
@@ -0,0 +1,16 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2025 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%--------------------------------------------------------------------
+
+-module(emqx_extsub_redis).
+
+-export([register_hooks/0, unregister_hooks/0]).
+
+-spec register_hooks() -> ok.
+register_hooks() ->
+    ok.
+
+-spec unregister_hooks() -> ok.
+unregister_hooks() ->
+    ok.
+
